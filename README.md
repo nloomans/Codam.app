76b35849867ccf4ac9a49a7a7bc95d6f3616532d
Euan <ehollidge@hotmail.com>
2019-10-18 18:27:03 +0200

    Install Script Sanitization and README

--- README.md
+++ README.md
@@ -3,10 +3,33 @@
 Codam.app is a repository of applications which are normally difficult to
 install on a Codam iMac because of the inability to mount `.dmg`s.
 
-## Downloading
+## Installing
+
+The recommended way to install an app is to use the installation script.
+Download the install.sh file from the git repository using the command below.
+
+```bash
+curl https://raw.githubusercontent.com/nloomans/Codam.app/Install-Script/install.sh > install.sh
+```
+
+You can this bash file to install programs using the following syntax.
+
+```bash
+sh install.sh <program name> [<application folder>]
+```
+
+## Manual Installation
 
 You can find the latest downloads on
 [the releases page.](https://github.com/nloomans/Codam.app/releases/latest/)
+Download the tar.gz file for your application. Extract into your desired
+installation directory. Once Extracted the program is installed. You can delete
+the tar.gz file. Double click on the .app folder/file to start the program.
+
+## Deletion
+
+You can delete the programs at any time by going to the install location and
+deleting the .app folder. By default this location is `~/Applications`.
 
 ## Requesting an .app
 
--- install.sh
+++ install.sh
@@ -1,35 +1,40 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# INIT
-DIR="/Users/$USER/Applications/"
-APP_NAME=$(echo $1 | awk '{print tolower($0)}')
-APP_NAME="$(tr '[:lower:]' '[:upper:]' <<< ${APP_NAME:0:1})${APP_NAME:1}"
-if [ $1 = "-controls" ]; then
-	echo "sh install.sh [App Name] [Absolute Install Location(optional)]"
+# Init
+APPLICATION_DIR="/Users/$USER/Applications/"
+APP_NAME="$(echo $1)"
+if [ "$1" = "--help" ]; then
+	echo "sh install.sh <program name> [<application folder>]"
 	exit 1
 fi
-VAL=$(curl https://api.github.com/repos/nloomans/Codam.app/releases/latest  --silent | grep 'browser_download_url' | grep $APP_NAME)
-if [ -z VAL ]; then
-	# not found
+if ! echo "$1" | grep -q -E '^[A-Za-z0-9]+$'; then
+	echo "program name failed regex [A-Za-z0-9 ]+"
+	exit 1
+fi
+
+# Locate
+echo "Locating package $APP_NAME"
+URL=$(curl https://api.github.com/repos/nloomans/Codam.app/releases/latest  --silent | grep 'browser_download_url' | grep -i $APP_NAME)
+if [ -z "$URL" ]; then
 	echo "App Not Supported"
 	exit 1
 fi
+URL=$(echo $URL | cut -f4 -d'"')
 
-# Work
-if  [[ ! -z $2 ]]; then
-	DIR= $(echo $2)
+# Install
+if  [[ ! -z "$2" ]]; then
+	APPLICATION_DIR="$2"
 fi
-cd $DIR
+cd $APPLICATION_DIR
 if [ $? -eq 1 ]; then
-	echo "Custom Location Does Not Exist"
-	echo "Choose One That Does"
+	echo "Custom location does not exist"
+	echo "Choose one that does"
 	exit 1
 fi
-VAL=$(echo $VAL | cut -f4 -d'"')
-echo "Downloading Package"
-curl -L $VAL > ${APP_NAME}.app.tar.gz
-echo "Package Downloaded"
-echo "Installing Package"
-tar -zxvf ${APP_NAME}.app.tar.gz
-rm ${APP_NAME}.app.tar.gz
+echo "Downloading $URL"
+TARBALL_NAME=$(basename $URL)
+curl -L $URL > ${TARBALL_NAME}
+echo "Extracting Package"
+tar -zxvf ${TARBALL_NAME}
+rm ${TARBALL_NAME}
 echo "Package Installed"
